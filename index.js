2451e13f694cbe18ad968e947616df2d5dc4ef6a
bkis <kiss@mailbox.org>
2020-03-25 17:23:13 +0100

    schmoof

--- index.js
+++ index.js
@@ -1,112 +1,132 @@
-$(function() {
+//// ROLL ANIMATION (thanks to https://stackoverflow.com/a/15191130)
+$.fn.animateRotate = function(angle, duration, easing, complete) {
+    var args = $.speed(duration, easing, complete);
+    var step = args.step;
+    return this.each(function(i, e) {
+    args.complete = $.proxy(args.complete, e);
+    args.step = function(now) {
+        $.style(e, 'transform', 'rotate(' + now + 'deg)');
+            if (step) return step.apply(e, arguments);
+        };
 
+        $({deg: 0}).animate({deg: angle}, args);
+    });
+};
+
+
+//// INITIALIZE
+$(function() {
     //// GENERAL CONSTANTS
     const imgDirPath = "img/";
     const imgNamePrefix = "d";
     const imgNameSuffix = ".png";
 
     //// TEMPLATE ELEMENTS
-
-    const templateDieStock = $("#templates > .die-stock").first();
     const templateDie = $("#templates > .die").first();
 
+    //// ADD DIE FUNCTION
+    const addDie = function(type) {
+        let die = templateDie.clone();
 
-    //// FUNCTIONS
+        // set data attribute
+        die.attr("data-type", type);
 
-    const render = function() {
-        this.domContainer.empty();
-        this.dice.forEach(die => this.domContainer.add(die));
-    }
-
-    const add = function(type) {
-        this.dice.push(die);
-        this.render();
-    }
-
-    const remove = function(type) {
-        this.dice.pop(crea);
-        this.render();
-        return die;
-    }
-
-    const clear = function() {
-        this.dice = [];
-        this.render();
-    }
-
-    const createContainer = function(domElement) {
-        return {
-            dice: [],
-            domElement: domElement,
-            add: add,
-            remove: remove,
-            render: render,
-            clear: clear
-        };
-    }
-
-    const createDie = function(container, type) {
-        return {
-            stock: container.isStock,
-            type: type,
-            value: -1,
-            container: container,
-            domElement: container.isStock
-                ? createStockDieElement(type)
-                : createDieElement(type)
-        };
-    }
-
-    const createDieElement = function(type) {
-        let e = templateDie.clone();
         // set image
-        e.children(".die-image").first()
-            .attr("src", imgDirPath + imgNamePrefix + type + imgNameSuffix)
-            .attr("alt", "d" + type);
+        die.children(".die-image").first()
+            .attr("src", imgDirPath + imgNamePrefix + type + imgNameSuffix);
+
+        //roll function
+        die.roll = function() {
+            die.animateRotate(90, {
+                duration: 1337,
+                easing: "linear",
+                complete: function() {},
+                step: function() {}
+            });
+        };
+
         // set initial value text
-        e.children(".die-value").first()
-            .text(" ");
+        die.children(".die-value").first().text(type);
+
         // button functionality: SELECT
-        e.children(".die-buttons > .btn-select").first().click(function() {
-            hand.add(table.remove(this));
+        die.click(function(e) {
+            e.stopPropagation();
+            die.toggleClass("active");
         });
+
         // button functionality: ROLL
-        e.children(".die-buttons > .btn-roll").first().click(function() {
-            this.roll();
+        die.find(".die-buttons > .btn-roll").first().click(function(e) {
+            // prevent click event bubbling up
+            event.stopPropagation();
+            // roll this and all active dice
+            let toRoll = die.hasClass("active") ? $(".active").add(die) : die;
+            toRoll.each(function(){
+                let currentDie = $(this);
+                currentDie.children(".die-value").first().text("?");
+                currentDie.children(".die-image").first().animateRotate(
+                    720,
+                    1500,
+                    "swing",
+                    function() {
+                        currentDie.children(".die-value").first()
+                            .text(Math.floor(Math.random() * parseInt(currentDie.attr("data-type"))) + 1);
+                    }
+                );
+            })
         });
+
         // button functionality: REMOVE
-        e.children(".die-buttons > .btn-remove").first().click(function() {
-            
+        die.find(".die-buttons > .btn-remove").first().click(function(e) {
+            // prevent click event bubbling up
+            event.stopPropagation();
+            die.remove();
         });
-    }
 
+        // hide buttons
+        die.children(".die-buttons").first().hide(0);
+        die.mouseenter(function() {
+            die.children(".die-buttons").first().show(100);
+        });
+        die.mouseleave(function() {
+            die.children(".die-buttons").first().hide(100);
+        });
 
-    //// INIT CONTAINERS
-
-    const hand  = createContainer($("#hand"));
-    const table = createContainer($("#table"));
-
-    
-    //// EVENTS
-
-
+        // add die to table
+        $("#table").append(die);
+    };
 
     //// INIT UI
 
     // init stock dice
-    $("#stock > .die-stock").each(function(i){
+    $("#stock > .die-stock").each(function(i) {
         // index
         $(this).attr("data-index", i);
         // title
-        $(this).attr("title", "Add d" + $(this).attr("data-type") + " to the table");
+        $(this).attr(
+            "title",
+            "Add d" + $(this).attr("data-type") + " to the table"
+        );
         // bg image
-        $(this).css("background-image", "url(" + imgDirPath + imgNamePrefix + $(this).attr("data-type") + imgNameSuffix + ")");
+        $(this).css(
+            "background-image",
+            "url(" +
+                imgDirPath +
+                imgNamePrefix +
+                $(this).attr("data-type") +
+                imgNameSuffix +
+                ")"
+        );
         // label
         $(this).text($(this).attr("data-type"));
         // click handler
-        $(this).click(function(){
-            alert("Clicked d" + $(this).attr("data-type"));
+        $(this).click(function(e) {
+            e.stopPropagation();
+            addDie(parseInt($(this).attr("data-type")));
         });
     });
-    
+
+    $("html").click(function(e) {
+        e.stopPropagation();
+        $(".die").removeClass("active");
+    });
 });
