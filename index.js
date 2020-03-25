23bee21df9f4ab3fcebee45c0666b05712aa736b
bkis <kiss@mailbox.org>
2020-03-25 17:23:07 +0100

    schmoof

--- index.js
+++ index.js
@@ -1,34 +1,112 @@
 $(function() {
 
+    //// GENERAL CONSTANTS
+    const imgDirPath = "img/";
+    const imgNamePrefix = "d";
+    const imgNameSuffix = ".png";
+
+    //// TEMPLATE ELEMENTS
+
+    const templateDieStock = $("#templates > .die-stock").first();
+    const templateDie = $("#templates > .die").first();
+
+
     //// FUNCTIONS
 
-    function addDieToNav(value){
-        
+    const render = function() {
+        this.domContainer.empty();
+        this.dice.forEach(die => this.domContainer.add(die));
     }
 
-    function addDiceToNav(values){
-        values.array.forEach( v => addDieToTable(v) );
+    const add = function(type) {
+        this.dice.push(die);
+        this.render();
     }
 
-    function addDieToTable(value){
-
+    const remove = function(type) {
+        this.dice.pop(crea);
+        this.render();
+        return die;
     }
 
-    function removeDieFromTable(die){
-
+    const clear = function() {
+        this.dice = [];
+        this.render();
     }
 
-    function addDieToHand(die){
-
+    const createContainer = function(domElement) {
+        return {
+            dice: [],
+            domElement: domElement,
+            add: add,
+            remove: remove,
+            render: render,
+            clear: clear
+        };
     }
 
-    function removeDieFromHand(die){
-
+    const createDie = function(container, type) {
+        return {
+            stock: container.isStock,
+            type: type,
+            value: -1,
+            container: container,
+            domElement: container.isStock
+                ? createStockDieElement(type)
+                : createDieElement(type)
+        };
     }
+
+    const createDieElement = function(type) {
+        let e = templateDie.clone();
+        // set image
+        e.children(".die-image").first()
+            .attr("src", imgDirPath + imgNamePrefix + type + imgNameSuffix)
+            .attr("alt", "d" + type);
+        // set initial value text
+        e.children(".die-value").first()
+            .text(" ");
+        // button functionality: SELECT
+        e.children(".die-buttons > .btn-select").first().click(function() {
+            hand.add(table.remove(this));
+        });
+        // button functionality: ROLL
+        e.children(".die-buttons > .btn-roll").first().click(function() {
+            this.roll();
+        });
+        // button functionality: REMOVE
+        e.children(".die-buttons > .btn-remove").first().click(function() {
+            
+        });
+    }
+
+
+    //// INIT CONTAINERS
+
+    const hand  = createContainer($("#hand"));
+    const table = createContainer($("#table"));
+
+    
+    //// EVENTS
+
 
 
     //// INIT UI
 
-    addDiceToNav([2, 4, 6, 8, 10, 12, 20, 30, 100]);
+    // init stock dice
+    $("#stock > .die-stock").each(function(i){
+        // index
+        $(this).attr("data-index", i);
+        // title
+        $(this).attr("title", "Add d" + $(this).attr("data-type") + " to the table");
+        // bg image
+        $(this).css("background-image", "url(" + imgDirPath + imgNamePrefix + $(this).attr("data-type") + imgNameSuffix + ")");
+        // label
+        $(this).text($(this).attr("data-type"));
+        // click handler
+        $(this).click(function(){
+            alert("Clicked d" + $(this).attr("data-type"));
+        });
+    });
     
 });
