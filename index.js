f8fc69d93c9689505a2e574e3ff17f7b45abca9a
bkis <kiss@mailbox.org>
2020-03-25 12:08:20 +0100

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+$(function() {
+
+    //// FUNCTIONS
+
+    function addDieToNav(value){
+        
+    }
+
+    function addDiceToNav(values){
+        values.array.forEach( v => addDieToTable(v) );
+    }
+
+    function addDieToTable(value){
+
+    }
+
+    function removeDieFromTable(die){
+
+    }
+
+    function addDieToHand(die){
+
+    }
+
+    function removeDieFromHand(die){
+
+    }
+
+
+    //// INIT UI
+
+    addDiceToNav([2, 4, 6, 8, 10, 12, 20, 30, 100]);
+    
+});
