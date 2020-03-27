9b4595c4f4cf4fb7cc1a886731aeb6b657f2127d
bkis <kiss@mailbox.org>
2020-03-27 10:13:16 +0100

    preload images

--- index.js
+++ index.js
@@ -6,6 +6,17 @@
  */
 
 
+
+//// GENERAL CONSTANTS
+const imgDirPath = "img/";
+const imgNamePrefix = "d";
+const imgNameSuffix = ".png";
+
+
+//// TEMPLATE ELEMENTS
+const templateDie = $("#templates > .die").first();
+
+
 //// ROLL ANIMATION (thanks to https://stackoverflow.com/a/15191130)
 $.fn.animateRotate = function(angle, duration, easing, complete) {
     var args = $.speed(duration, easing, complete);
@@ -22,6 +33,36 @@ $.fn.animateRotate = function(angle, duration, easing, complete) {
 };
 
 
+//// IMAGES AND IMAGE-PRELOADING
+var images = {
+    files: {
+        d4: "d4.png",
+        d6: "d6.png",
+        d8: "d8.png",
+        d10: "d10.png",
+        d12: "d12.png",
+        d20: "d20.png",
+        d100: "d100.png",
+        iconSoundOn: "icon-sound-on.png",
+        iconSoundOff: "icon-sound-off.png",
+        iconGitHub: "icon-gh.png",
+        iconRoll: "icon-roll.png",
+        iconRemove: "icon-remove.png",
+        iconAdd: "icon-add.png"
+    },
+    path: function(id){ return imgDirPath + this.files[id]; },
+    allPaths: function(){ return Object.keys(this.files).map(k => this.path(k)); }
+}
+
+// define and call (!) preloading of images
+var preLoadImages = function(imagesArray) {
+    $('<div id="loaded-images">').css('display','none').appendTo('body');
+    $(imagesArray).each(function () {
+        $('<img />').attr('src', this).appendTo('#loaded-images');
+    });
+}(images.allPaths());
+
+
 //// preps
 
 var total = 0;
@@ -36,8 +77,9 @@ function toggleSound(){
     $("#sound-toggle").css(
         "background-image",
         sound
-            ? "url(img/icon-sound-on.png)"
-            : "url(img/icon-sound-off.png)");
+            ? "url(" + images.path("iconSoundOn") + ")"
+            : "url(" + images.path("iconSoundOff") + ")"
+    );
 }
 
 // fnc to play dice rolling sound
@@ -58,90 +100,84 @@ function resetDieValues(){
 }
 
 
+//// ADD DIE FUNCTION
+function addDie(type) {
+    let die = templateDie.clone();
+
+    // set data attribute
+    die.attr("data-type", type);
+
+    // set image
+    die.find(".rotatable > .die-image").first().css(
+        "background-image",
+        "url(" + imgDirPath + imgNamePrefix + type + imgNameSuffix + ")"
+    );
+
+    // set initial value text
+    die.find(".rotatable > .die-value").first().text(type);
+
+    // button functionality: SELECT
+    die.click(function(e) {
+        e.stopPropagation();
+        die.toggleClass("active");
+    });
+
+    // button functionality: ROLL
+    die.find(".die-buttons > .btn-roll").first().click(function(e) {
+        // prevent click event bubbling up
+        event.stopPropagation();
+        //play sound
+        playRollSound();
+        //reset total
+        total = 0;
+        $("#total-value").text("?").animateRotate(720, 1000, "swing");
+        //reset all dice to their natural max value
+        resetDieValues();
+        // roll this and all active dice
+        let toRoll = die.hasClass("active") ? $(".active").add(die) : die;
+        toRoll.each(function(){
+            let currentDie = $(this);
+            currentDie.find(".rotatable > .die-value").first().text("?");
+            currentDie.children(".rotatable").first().animateRotate(
+                720,
+                1000,
+                "swing",
+                function() {
+                    let result = Math.floor(Math.random() * parseInt(currentDie.attr("data-type"))) + 1;
+                    total += parseInt(result);
+                    currentDie.find(".rotatable > .die-value").first().text(result);
+                    $("#total-value").text(total);
+                }
+            );
+        });
+    });
+
+    // button functionality: REMOVE
+    die.find(".die-buttons > .btn-remove").first().click(function(e) {
+        // prevent click event bubbling up
+        event.stopPropagation();
+        die.remove();
+    });
+
+    // hide die buttons initially
+    die.children(".die-buttons").first().hide(0);
+    // show die buttons on mouse enter
+    die.mouseenter(function() {
+        die.children(".die-buttons").first().show(100);
+    });
+    // hide die buttons on mouse leave
+    die.mouseleave(function() {
+        die.children(".die-buttons").first().hide(100);
+    });
+
+    // add die to table
+    $("#table").append(die);
+};
+
+
 //// INITIALIZE
+
 $(function() {
-    //// GENERAL CONSTANTS
-    const imgDirPath = "img/";
-    const imgNamePrefix = "d";
-    const imgNameSuffix = ".png";
-
-    //// TEMPLATE ELEMENTS
-    const templateDie = $("#templates > .die").first();
-
-    //// ADD DIE FUNCTION
-    const addDie = function(type) {
-        let die = templateDie.clone();
-
-        // set data attribute
-        die.attr("data-type", type);
-
-        // set image
-        die.find(".rotatable > .die-image").first().css(
-            "background-image",
-            "url(" + imgDirPath + imgNamePrefix + type + imgNameSuffix + ")"
-        );
-
-        // set initial value text
-        die.find(".rotatable > .die-value").first().text(type);
-
-        // button functionality: SELECT
-        die.click(function(e) {
-            e.stopPropagation();
-            die.toggleClass("active");
-        });
-
-        // button functionality: ROLL
-        die.find(".die-buttons > .btn-roll").first().click(function(e) {
-            // prevent click event bubbling up
-            event.stopPropagation();
-            //play sound
-            playRollSound();
-            //reset total
-            total = 0;
-            $("#total-value").text("?").animateRotate(720, 1000, "swing");
-            //reset all dice to their natural max value
-            resetDieValues();
-            // roll this and all active dice
-            let toRoll = die.hasClass("active") ? $(".active").add(die) : die;
-            toRoll.each(function(){
-                let currentDie = $(this);
-                currentDie.find(".rotatable > .die-value").first().text("?");
-                currentDie.children(".rotatable").first().animateRotate(
-                    720,
-                    1000,
-                    "swing",
-                    function() {
-                        let result = Math.floor(Math.random() * parseInt(currentDie.attr("data-type"))) + 1;
-                        total += parseInt(result);
-                        currentDie.find(".rotatable > .die-value").first().text(result);
-                        $("#total-value").text(total);
-                    }
-                );
-            });
-        });
-
-        // button functionality: REMOVE
-        die.find(".die-buttons > .btn-remove").first().click(function(e) {
-            // prevent click event bubbling up
-            event.stopPropagation();
-            die.remove();
-        });
-
-        // hide die buttons initially
-        die.children(".die-buttons").first().hide(0);
-        // show die buttons on mouse enter
-        die.mouseenter(function() {
-            die.children(".die-buttons").first().show(100);
-        });
-        // hide die buttons on mouse leave
-        die.mouseleave(function() {
-            die.children(".die-buttons").first().hide(100);
-        });
-
-        // add die to table
-        $("#table").append(die);
-    };
-
 
     //// INIT UI
 
